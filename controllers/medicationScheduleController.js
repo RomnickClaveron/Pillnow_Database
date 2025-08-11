28bb894819cc5647de8ee76f5f5e1008b6c2f5e7
RomnickClaveron <nickclaveron@gmail.com>
2025-08-12 01:37:58 +0800

    Update medicationScheduleController.js
    s

--- controllers/medicationScheduleController.js
+++ controllers/medicationScheduleController.js
@@ -3,16 +3,21 @@ const MedicationSchedule = require('../models/medication_scheduleModels');
 // Create a new medication schedule
 exports.createMedicationSchedule = async (req, res) => {
     try {
+        console.log('Request body:', req.body);
+        console.log('Request headers:', req.headers);
+        console.log('Content-Type:', req.headers['content-type']);
+        
         const schedule = new MedicationSchedule({
-            userId: req.body.userId,
-            medicationId: req.body.medicationId,
-            scheduleTime: req.body.scheduleTime,
-            frequency: req.body.frequency,
-            status: req.body.status
+            user: req.body.user,
+            medication: req.body.medication,
+            time: req.body.time,
+            status: req.body.status || 'Pending',
+            alertSent: req.body.alertSent || false
         });
         const savedSchedule = await schedule.save();
         res.status(201).json(savedSchedule);
     } catch (error) {
+        console.log('Error:', error.message);
         res.status(400).json({ message: error.message });
     }
 };
@@ -21,8 +26,8 @@ exports.createMedicationSchedule = async (req, res) => {
 exports.getAllMedicationSchedules = async (req, res) => {
     try {
         const schedules = await MedicationSchedule.find()
-            .populate('userId')
-            .populate('medicationId');
+            .populate('user')
+            .populate('medication');
         res.status(200).json(schedules);
     } catch (error) {
         res.status(500).json({ message: error.message });
@@ -32,9 +37,9 @@ exports.getAllMedicationSchedules = async (req, res) => {
 // Get medication schedules by user ID
 exports.getMedicationSchedulesByUserId = async (req, res) => {
     try {
-        const schedules = await MedicationSchedule.find({ userId: req.params.userId })
-            .populate('userId')
-            .populate('medicationId');
+        const schedules = await MedicationSchedule.find({ user: req.params.userId })
+            .populate('user')
+            .populate('medication');
         res.status(200).json(schedules);
     } catch (error) {
         res.status(500).json({ message: error.message });
@@ -44,9 +49,9 @@ exports.getMedicationSchedulesByUserId = async (req, res) => {
 // Get medication schedules by medication ID
 exports.getMedicationSchedulesByMedicationId = async (req, res) => {
     try {
-        const schedules = await MedicationSchedule.find({ medicationId: req.params.medicationId })
-            .populate('userId')
-            .populate('medicationId');
+        const schedules = await MedicationSchedule.find({ medication: req.params.medicationId })
+            .populate('user')
+            .populate('medication');
         res.status(200).json(schedules);
     } catch (error) {
         res.status(500).json({ message: error.message });
@@ -61,8 +66,8 @@ exports.updateMedicationSchedule = async (req, res) => {
             req.body,
             { new: true }
         )
-        .populate('userId')
-        .populate('medicationId');
+        .populate('user')
+        .populate('medication');
         res.status(200).json(updatedSchedule);
     } catch (error) {
         res.status(400).json({ message: error.message });
